0b3f97e755063625623aeed2c1e71bb1ce498c54
Victor Abreu <aalevictor@gmail.com>
2024-04-12 13:55:52 -0300

    remover arquivos

--- src/alvara_tipo/dto/update-alvara_tipo.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAlvaraTipoDto } from './create-alvara_tipo.dto';
-
-export class UpdateAlvaraTipoDto extends PartialType(CreateAlvaraTipoDto) {}
--- src/iniciais/dto/update-inicial.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateInicialDto } from './create-inicial.dto';
-
-export class UpdateInicialDto extends PartialType(CreateInicialDto) {}
