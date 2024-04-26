807a3f571df5727d0c0d3c6ea21a718938d435a2
Victor Abreu <aalevictor@gmail.com>
2024-04-26 09:54:57 -0300

    admissibilidade

--- src/inicial/dto/create-inicial.dto.ts
+++ src/inicial/dto/create-inicial.dto.ts
@@ -29,4 +29,31 @@ export class CreateInicialDto {
     status?: number;
     @ApiProperty()
     nums_sql?: string[];
+    @ApiProperty()
+    interfaces?: any;
+}
+
+export class CreateInterfacesDto {
+  @ApiProperty()
+  inicial_id?: number;
+  @ApiProperty()
+  interface_sehab?: boolean = false;
+  @ApiProperty()
+  interface_siurb?: boolean = false;
+  @ApiProperty()
+  interface_smc?: boolean = false;
+  @ApiProperty()
+  interface_smt?: boolean = false;
+  @ApiProperty()
+  interface_svma?: boolean = false;
+  @ApiProperty()
+  num_sehab?: string;
+  @ApiProperty()
+  num_siurb?: string;
+  @ApiProperty()
+  num_smc?: string;
+  @ApiProperty()
+  num_smt?: string;
+  @ApiProperty()
+  num_svma?: string;
 }
--- src/inicial/inicial.service.ts
+++ src/inicial/inicial.service.ts
@@ -1,5 +1,5 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { CreateInicialDto } from './dto/create-inicial.dto';
+import { CreateInicialDto, CreateInterfacesDto } from './dto/create-inicial.dto';
 import { UpdateInicialDto } from './dto/update-inicial.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Inicial } from '@prisma/client';
@@ -75,10 +75,14 @@ export class InicialService {
     const prazo = prazo_admissibilidade + prazo_analise_multi1;
     const data_original = this.adicionaDiasData(inicial.data_protocolo, prazo);
     var data_reuniao = this.pegaQuarta(data_original);
-    const reuniao = await this.prisma.reuniao_Processo.create({
-      data: {
+    const reuniao = await this.prisma.reuniao_Processo.upsert({
+      where: { inicial_id: inicial.id },
+      create: {
         data_reuniao,
         inicial_id: inicial.id
+      },
+      update: {
+        data_reuniao
       }
     });
     if (!reuniao) throw new ForbiddenException('Erro ao gerar reunião.');
@@ -98,17 +102,133 @@ export class InicialService {
     return true;
   }
 
+  async criaInterfaces(interfaces: CreateInterfacesDto, inicial_id: number) {
+    const interfaceUpsert = await this.prisma.interface.upsert({
+      where: { inicial_id },
+      create: {
+        inicial_id,
+        ...interfaces
+      },
+      update: {
+        ...interfaces
+      }
+    });
+    if (!interfaceUpsert) throw new ForbiddenException('Erro ao criar interface.');
+  }
+
+  async alocaResponsavelTecnico(inicial: Inicial) {
+    const inicial_id = inicial.id;
+    const distribuicao = await this.prisma.distribuicao.findFirst({ where: { inicial_id } });
+    if (!distribuicao) throw new ForbiddenException('Erro ao buscar distribuição.');
+    const agora = new Date();
+    var tecnico_responsavel_id = '';
+    const include = {
+      ferias: {
+        where: { OR: [
+            {
+              inicio: {
+                gte: agora,
+                lte: this.adicionaDiasData(agora, 7)
+              }
+            }, {
+              inicio: { lte: agora },
+              final: { gte: agora }
+            }
+        ]}
+      }
+    };
+    var tecnicos = await this.prisma.usuario.findMany({
+      where: { 
+        cargo: 'TEC'
+      },
+      orderBy: {
+        criado_em: 'asc'
+      },
+      include
+    });
+    var tecnicos_id = tecnicos.filter(admin => admin.ferias.length === 0).map(admin => admin.id);
+    var ultimo_tec = await this.prisma.distribuicao.findFirst({
+      where: { NOT: { tecnico_responsavel_id: null }},
+      orderBy: { criado_em: 'desc' }
+    });
+    if (!ultimo_tec) tecnico_responsavel_id = tecnicos_id[0];
+    else {
+      tecnico_responsavel_id = ultimo_tec.tecnico_responsavel_id;
+      var ultimo_tec_index = tecnicos_id.findIndex(id => id === tecnico_responsavel_id);
+      ultimo_tec_index = ultimo_tec_index === tecnicos_id.length - 1 ? 0 : (ultimo_tec_index + 1);
+      tecnico_responsavel_id = tecnicos_id[ultimo_tec_index];
+    }
+    const distribuicao_tecnico = await this.prisma.distribuicao.update({
+      where: { inicial_id },
+      data: {
+        tecnico_responsavel_id
+      }
+    });
+    if (!distribuicao_tecnico) throw new ForbiddenException('Erro ao alocar técnico.');
+  }
+
+  async criaDistribuicao(inicial: Inicial) {
+    const agora = new Date();
+    var administrativo_responsavel_id = '';
+    const include = {
+      ferias: {
+        where: { OR: [
+            {
+              inicio: {
+                gte: agora,
+                lte: this.adicionaDiasData(agora, 7)
+              }
+            }, {
+              inicio: { lte: agora },
+              final: { gte: agora }
+            }
+        ]}
+      }
+    };
+    var administrativos = await this.prisma.usuario.findMany({
+      where: { 
+        cargo: 'ADM'
+      },
+      orderBy: {
+        criado_em: 'asc'
+      },
+      include
+    });
+    var administrativosId = administrativos.filter(admin => admin.ferias.length === 0).map(admin => admin.id);
+    var ultimo_adm = await this.prisma.distribuicao.findFirst({ orderBy: { criado_em: 'desc' }});
+    if (!ultimo_adm) administrativo_responsavel_id = administrativosId[0];
+    else {
+      administrativo_responsavel_id = ultimo_adm.administrativo_responsavel_id;
+      var ultimo_adm_index = administrativosId.findIndex(id => id === administrativo_responsavel_id);
+      ultimo_adm_index = ultimo_adm_index === administrativosId.length - 1 ? 0 : (ultimo_adm_index + 1);
+      administrativo_responsavel_id = administrativosId[ultimo_adm_index];
+    }
+    const distribuicao = await this.prisma.distribuicao.create({
+      data: {
+        inicial_id: inicial.id,
+        administrativo_responsavel_id
+      }
+    });
+    if (!distribuicao) throw new ForbiddenException('Erro ao criar distribuição.');
+    if (inicial.envio_admissibilidade && distribuicao) {
+      await this.alocaResponsavelTecnico(inicial);
+    }
+  }
+
   async criar(createInicialDto: CreateInicialDto): Promise<Inicial> {
-    const { nums_sql } = createInicialDto;
+    const { nums_sql, interfaces } = createInicialDto;
     delete createInicialDto.nums_sql;
+    delete createInicialDto.interfaces;
+    if (createInicialDto.envio_admissibilidade) createInicialDto.status = 2;
     const novo_inicial = await this.prisma.inicial.create({
       data: { ...createInicialDto },
     });
     if (!novo_inicial) throw new ForbiddenException('Erro ao criar processo');
-    console.log(novo_inicial.tipo_processo);
     if (novo_inicial.tipo_processo === 2){
       await this.geraReuniaoData(novo_inicial);
+      await this.criaInterfaces(interfaces as CreateInterfacesDto, novo_inicial.id);
     }
+    if (novo_inicial) await this.criaDistribuicao(novo_inicial);
     if (nums_sql && nums_sql.length > 0) {
       await this.prisma.inicial_Sqls.createMany({
         data: nums_sql.map(sql => ({ sql, inicial_id: novo_inicial.id })),
@@ -144,6 +264,14 @@ export class InicialService {
       where: { id },
       include: {
         iniciais_sqls: true,
+        interfaces: true,
+        admissibilidade: true,
+        distribuicao: {
+          include: {
+            administrativo_responsavel: true,
+            tecnico_responsavel: true
+          }
+        }
       }
     });
     if (!inicial) throw new ForbiddenException('Nenhum processo encontrado');
@@ -156,12 +284,15 @@ export class InicialService {
   ): Promise<Inicial> {
     const inicial = await this.prisma.inicial.findUnique({ where: { id } });
     if (!inicial) throw new ForbiddenException('Nenhum processo encontrado');
+    const { interfaces } = updateInicialDto;
+    delete updateInicialDto.interfaces;
     const inicial_atualizado = await this.prisma.inicial.update({
       where: { id },
       data: { ...updateInicialDto },
     });
     if (inicial_atualizado.tipo_processo === 2){
       await this.geraReuniaoData(inicial_atualizado);
+      await this.criaInterfaces(interfaces as CreateInterfacesDto, inicial_atualizado.id);
     }
     if (!inicial_atualizado)
       throw new ForbiddenException('Erro ao atualizar processo');
