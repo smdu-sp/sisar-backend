d4d281de506a8afcef28fd5e6c604e78c40dbccf
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-09-06 14:32:42 -0300

    criada documentação do swagger para unidades controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ import { IsPublic } from './decorators/is-public.decorator';
 import { UsuarioAtual } from './decorators/usuario-atual.decorator';
 import { Usuario } from '@prisma/client';
 import { RefreshAuthGuard } from './guards/refresh.guard';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsuarioToken } from './models/UsuarioToken';
 import { LoginDto } from './models/login.dto';
 
@@ -25,15 +25,10 @@ export class AuthController {
 
   @Post('login') //localhost:3000/login
   @HttpCode(HttpStatus.OK)
-  @ApiBody({
-    description: 'Senha e login para autenticação por JWT',
-    type: LoginDto
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Retorna 200 se tiver sucesso no login.',
-    type: UsuarioToken
-  })
+  @ApiBody({ description: 'Senha e login para autenticação por JWT.', type: LoginDto })
+  @ApiResponse({ status: 200, description: 'Retorna 200 se tiver sucesso no login.', type: UsuarioToken })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Logue na aplicação.", summary: 'Faça o login na aplicação.' })
   @UseGuards(LocalAuthGuard)
   @IsPublic()
   login(@Request() req: AuthRequest) {
@@ -41,10 +36,9 @@ export class AuthController {
   }
 
   @Post('refresh') //localhost:3000/refresh
-  @ApiResponse({
-    status: 200,
-    description: 'Retorna 200 se o token for atualizado.',
-  })
+  @ApiResponse({ status: 200, description: 'Retorna 200 se o token for atualizado.' })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Faça o refresh de seu token.", summary: 'Faça refresh do token.' })
   @IsPublic()
   @UseGuards(RefreshAuthGuard)
   refresh(@UsuarioAtual() usuario: Usuario) {
@@ -52,10 +46,9 @@ export class AuthController {
   }
 
   @Get('eu')
-  @ApiResponse({
-    status: 200,
-    description: 'Retorna 200 se o sistema encontrar o usuário logado.',
-  })
+  @ApiResponse({ status: 200, description: 'Retorna 200 se o sistema encontrar o usuário logado.' })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Busque o usuário logado.", summary: 'Busque o usuário logado' })
   usuarioAtual(@UsuarioAtual() usuario: Usuario) {
     return usuario;
   }
--- src/unidades/unidades.controller.ts
+++ src/unidades/unidades.controller.ts
@@ -1,10 +1,10 @@
 import { IsPublic } from './../auth/decorators/is-public.decorator';
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpCode, HttpStatus } from '@nestjs/common';
 import { UnidadesService } from './unidades.service';
 import { CreateUnidadeDto } from './dto/create-unidade.dto';
 import { UpdateUnidadeDto } from './dto/update-unidade.dto';
 import { Permissoes } from 'src/auth/decorators/permissoes.decorator';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Unidades')
 @Controller('unidades')
@@ -13,12 +13,25 @@ export class UnidadesController {
 
   @Permissoes('SUP', 'ADM')
   @Post('criar')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiBody({ description: 'Corpo da requisição para criação de unidade.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 201, description: 'Retorna 201 se criar com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Crie uma unidade.", summary: 'Crie unidades.' })
   criar(@Body() createUnidadeDto: CreateUnidadeDto) {
     return this.unidadesService.criar(createUnidadeDto);
   }
 
   @Permissoes('SUP', 'ADM')
   @Get('buscar-tudo')
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({ status: 200, description: 'Retorna 200 se buscar com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Busque todas as unidades.", summary: 'Busque todas as unidades.' })
+  @ApiQuery({ name: 'pagina', type: 'string', required: false })
+  @ApiQuery({ name: 'limite', type: 'string', required: false })
+  @ApiQuery({ name: 'filtro', type: 'string', required: false })
+  @ApiQuery({ name: 'busca', type: 'string', required: false })
   buscarTudo(
     @Query('pagina') pagina?: string,
     @Query('limite') limite?: string,
@@ -30,24 +43,42 @@ export class UnidadesController {
 
   @Permissoes('SUP', 'ADM')
   @Get('lista-completa')
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({ status: 200, description: 'Retorna 200 se listar tudo com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Listar todas as unidades.", summary: 'Liste todas as unidades.' })
   listaCompleta() {
     return this.unidadesService.listaCompleta();
   }
 
   @Permissoes('SUP', 'ADM')
   @Get('buscar-por-id/:id')
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({ status: 200, description: 'Retorna 200 se buscar por ID com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Buscar uma unidade.", summary: 'Busque uma unidade.' })
   buscarPorId(@Param('id') id: string) {
     return this.unidadesService.buscarPorId(id);
   }
 
   @Permissoes('SUP', 'ADM')
   @Patch('atualizar/:id')
+  @HttpCode(HttpStatus.OK)
+  @ApiBody({ description: 'Corpo da requisição para atualização de unidade.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 200, description: 'Retorna 200 se atualizar com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Atualizar uma unidade.", summary: 'Atualize uma unidade.' })
   atualizar(@Param('id') id: string, @Body() updateUnidadeDto: UpdateUnidadeDto) {
     return this.unidadesService.atualizar(id, updateUnidadeDto);
   }
 
   @Permissoes('SUP', 'ADM')
   @Patch('desativar/:id')
+  @HttpCode(HttpStatus.OK)
+  @ApiBody({ description: 'Corpo da requisição para desativação de unidade.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 200, description: 'Retorna 200 se desativar com sucesso.', type: CreateUnidadeDto })
+  @ApiResponse({ status: 401, description: 'Retorna 401 se não autorizado.' })
+  @ApiOperation({ description: "Desativar uma unidade.", summary: 'Desative uma unidade.' })
   desativar(@Param('id') id: string, @Body() updateUnidadeDto: UpdateUnidadeDto) {
     return this.unidadesService.desativar(id, updateUnidadeDto);
   }
