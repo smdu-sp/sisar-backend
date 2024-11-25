151b6d6c2bdf0b44a9300bbf6c31e7eb6d426341
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-11-25 14:50:30 -0300

    refatorando tipagem no serviço de subprefeituras

--- src/subprefeitura/subprefeitura.service.ts
+++ src/subprefeitura/subprefeitura.service.ts
@@ -3,7 +3,8 @@ import { CreateSubprefeituraDto } from './dto/create-subprefeitura.dto';
 import { UpdateSubprefeituraDto } from './dto/update-subprefeitura.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AppService } from 'src/app.service';
-import { CreateResponseSubprefeituraDTO } from './dto/subprefeitura-response.dto';
+import { CreateResponseSubprefeituraDTO, SubprefeituraResponseDTO } from './dto/subprefeitura-response.dto';
+import { Subprefeitura, Unidade } from '@prisma/client';
 
 @Injectable()
 export class SubprefeituraService {
@@ -12,25 +13,27 @@ export class SubprefeituraService {
     private app: AppService
   ) {}
 
-  async listaCompleta() {
-    const lista = await this.prisma.subprefeitura.findMany({
+  async listaCompleta(): Promise<SubprefeituraResponseDTO[]> {
+    const lista: Subprefeitura[] = await this.prisma.subprefeitura.findMany({
       orderBy: { nome: 'asc' }
     });
     return lista;
   }
 
-  async buscaPorNome(nome: string) {
-    const subprefeitura = await this.prisma.subprefeitura.findUnique({
+  async buscaPorNome(nome: string): Promise<SubprefeituraResponseDTO> {
+    const subprefeitura: Subprefeitura = await this.prisma.subprefeitura.findUnique({
       where: { nome }
     });
     return subprefeitura;
   }
 
-  async criar(createsubprefeituraDto: CreateSubprefeituraDto): Promise<CreateResponseSubprefeituraDTO> {
+  async criar(
+    createsubprefeituraDto: CreateSubprefeituraDto
+  ): Promise<CreateResponseSubprefeituraDTO> {
     const { nome, sigla, status } = createsubprefeituraDto;
     if (await this.buscaPorNome(nome)) 
       throw new ForbiddenException('Ja existe uma subprefeitura com o mesmo nome');
-    const novasubprefeitura = await this.prisma.subprefeitura.create({
+    const novasubprefeitura: Subprefeitura = await this.prisma.subprefeitura.create({
       data: { nome, sigla, status }
     });
     if (!novasubprefeitura) 
@@ -42,7 +45,12 @@ export class SubprefeituraService {
     pagina: number = 1,
     limite: number = 10,
     busca?: string
-  ) {
+  ): Promise<{ 
+    total: number, 
+    pagina: number, 
+    limite: number, 
+    data: Subprefeitura[] 
+  }> {
     [pagina, limite] = this.app.verificaPagina(pagina, limite);
     const searchParams = {
       ...(busca ? 
@@ -52,10 +60,10 @@ export class SubprefeituraService {
         ] } : 
         {}),
     };
-    const total = await this.prisma.subprefeitura.count({ where: searchParams });
-    if (total == 0) return { total: 0, pagina: 0, limite: 0, users: [] };
+    const total: number = await this.prisma.subprefeitura.count({ where: searchParams });
+    if (total == 0) return { total: 0, pagina: 0, limite: 0, data: [] };
     [pagina, limite] = this.app.verificaLimite(pagina, limite, total);
-    const subprefeituras = await this.prisma.subprefeitura.findMany({
+    const subprefeituras: Subprefeitura[] = await this.prisma.subprefeitura.findMany({
       where: searchParams,
       skip: (pagina - 1) * limite,
       take: limite,
@@ -68,22 +76,26 @@ export class SubprefeituraService {
     };
   }
 
-  async buscarPorId(id: string) {
-    const subprefeitura = await this.prisma.subprefeitura.findUnique({ where: { id } });
+  async buscarPorId(id: string): Promise<SubprefeituraResponseDTO> {
+    const subprefeitura: SubprefeituraResponseDTO = await this.prisma.subprefeitura.findUnique({ 
+      where: { id } 
+    });
     if (!subprefeitura) throw new ForbiddenException('subprefeitura não encontrada.');
     return subprefeitura;
   }
 
-  async atualizar(id: string, updatesubprefeituraDto: UpdateSubprefeituraDto) {
+  async atualizar(id: string, updatesubprefeituraDto: UpdateSubprefeituraDto): Promise<SubprefeituraResponseDTO> {
     const { nome } = updatesubprefeituraDto;
-    const subprefeitura = await this.prisma.subprefeitura.findUnique({ where: { id } });
+    const subprefeitura: SubprefeituraResponseDTO = await this.prisma.subprefeitura.findUnique({ 
+      where: { id } 
+    });
     if (!subprefeitura) throw new ForbiddenException('subprefeitura não encontrada.');
     if (nome) {
-      const subprefeituraNome = await this.buscaPorNome(nome);
+      const subprefeituraNome: SubprefeituraResponseDTO = await this.buscaPorNome(nome);
       if (subprefeituraNome && subprefeituraNome.id != id) 
         throw new ForbiddenException('Já existe uma subprefeitura com o mesmo nome.');
     }
-    const updatedsubprefeitura = await this.prisma.subprefeitura.update({
+    const updatedsubprefeitura: SubprefeituraResponseDTO = await this.prisma.subprefeitura.update({
       where: { id },
       data: updatesubprefeituraDto
     });
@@ -92,10 +104,10 @@ export class SubprefeituraService {
     return updatedsubprefeitura;
   }
 
-  async desativar(id: string) {
-    const unidade = await this.prisma.unidade.findUnique({ where: { id } });
+  async desativar(id: string): Promise<{ message: string }> {
+    const unidade: Unidade = await this.prisma.unidade.findUnique({ where: { id } });
     if (!unidade) throw new ForbiddenException('Unidade não encontrada.');
-    const updatedUnidade = await this.prisma.unidade.update({
+    const updatedUnidade: Unidade = await this.prisma.unidade.update({
       where: { id },
       data: { status: 0 }
     });
