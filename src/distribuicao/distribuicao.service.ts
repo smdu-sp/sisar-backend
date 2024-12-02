43c1af708b82965164dd5d8a0dff2f7305a4247d
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-12-02 14:26:32 -0300

    tipando serviços de distribuição

--- src/distribuicao/distribuicao.service.ts
+++ src/distribuicao/distribuicao.service.ts
@@ -2,26 +2,27 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateDistribuicaoDto } from './dto/create-distribuicao.dto';
 import { UpdateDistribuicaoDto } from './dto/update-distribuicao.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { DistribuicaoResponseDTO } from './dto/distribuicao-response.dto';
+import { Distribuicao } from '@prisma/client';
 
 @Injectable()
 export class DistribuicaoService {
-  constructor(
-    private prisma: PrismaService,
-  ) {}
-  async criar(createDistribuicaoDto: CreateDistribuicaoDto) {
-    const distribuicao = await this.prisma.distribuicao.findUnique({
+  constructor(private prisma: PrismaService) {}
+
+  async criar(createDistribuicaoDto: CreateDistribuicaoDto): Promise<DistribuicaoResponseDTO> {
+    const distribuicao: Distribuicao = await this.prisma.distribuicao.findUnique({
       where: { inicial_id: createDistribuicaoDto.inicial_id }
     });
     if (distribuicao) throw new ForbiddenException('Erro ao criar distribuição. Processo já foi alocado.');
-    const nova_distribuicao = await this.prisma.distribuicao.create({
+    const nova_distribuicao: Distribuicao = await this.prisma.distribuicao.create({
       data: createDistribuicaoDto
     });
     if (!nova_distribuicao) throw new ForbiddenException('Erro ao criar distribuição. Tente novamente.');
     return nova_distribuicao;
   }
 
-  async atualizar(inicial_id: number, updateDistribuicaoDto: UpdateDistribuicaoDto) {
-    const distribuicao = await this.prisma.distribuicao.findUnique({
+  async atualizar(inicial_id: number, updateDistribuicaoDto: UpdateDistribuicaoDto): Promise<DistribuicaoResponseDTO> {
+    const distribuicao: Distribuicao = await this.prisma.distribuicao.findUnique({
       where: { inicial_id: inicial_id },
       include: {
         tecnico_responsavel: true,
@@ -30,23 +31,23 @@ export class DistribuicaoService {
     });
     if (distribuicao) throw new ForbiddenException('Erro ao atualizar distribuição. Processo não existe.');
     const { administrativo_responsavel_id, tecnico_responsavel_id } = updateDistribuicaoDto;
-    const atualiza_distribuicao = await this.prisma.distribuicao.update({
+    const atualiza_distribuicao: Distribuicao = await this.prisma.distribuicao.update({
       where: { inicial_id },
       data: {
         ...(administrativo_responsavel_id && { administrativo_responsavel_id }),
         ...(tecnico_responsavel_id && { tecnico_responsavel_id })
       }
-    }).catch(error => console.log(error));
+    });
     if (!atualiza_distribuicao) throw new ForbiddenException('Erro ao atualizar distribuição. Tente novamente.');
     return atualiza_distribuicao;
   }
 
-  async mudarAdministrativoResponsavel(inicial_id: number, administrativo_responsavel_id: string) {
-    const distribuicao = await this.prisma.distribuicao.findUnique({
+  async mudarAdministrativoResponsavel(inicial_id: number, administrativo_responsavel_id: string): Promise<DistribuicaoResponseDTO> {
+    const distribuicao: Distribuicao = await this.prisma.distribuicao.findUnique({
       where: { inicial_id: inicial_id }
     });
     if (!distribuicao) throw new ForbiddenException('Erro ao atualizar distribuição. Processo não existe.');
-    const atualiza_distribuicao = await this.prisma.distribuicao.update({
+    const atualiza_distribuicao: Distribuicao = await this.prisma.distribuicao.update({
       where: { inicial_id },
       data: {
         administrativo_responsavel_id
@@ -56,16 +57,14 @@ export class DistribuicaoService {
     return atualiza_distribuicao;
   }
 
-  async mudarTecnicoResponsavel(inicial_id: number, tecnico_responsavel_id: string) {
-    const distribuicao = await this.prisma.distribuicao.findUnique({
+  async mudarTecnicoResponsavel(inicial_id: number, tecnico_responsavel_id: string): Promise<DistribuicaoResponseDTO> {
+    const distribuicao: Distribuicao = await this.prisma.distribuicao.findUnique({
       where: { inicial_id: inicial_id }
     });
     if (!distribuicao) throw new ForbiddenException('Erro ao atualizar distribuição. Processo não existe.');
-    const atualiza_distribuicao = await this.prisma.distribuicao.update({
+    const atualiza_distribuicao: Distribuicao = await this.prisma.distribuicao.update({
       where: { inicial_id },
-      data: {
-        tecnico_responsavel_id
-      }
+      data: { tecnico_responsavel_id }
     })
     if (!atualiza_distribuicao) throw new ForbiddenException('Erro ao atualizar distribuição. Tente novamente.');
     return atualiza_distribuicao;
