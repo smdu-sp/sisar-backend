64fe2f8aed7ba0fdd0d663cc4541fcafcc36882c
fernando <anjosdelacerda@gmail.com>
2025-02-05 11:56:30 -0300

    feat/implementação de teste de listagem de reuniões

--- src/reunioes/tests/reunioes.service.spec.ts
+++ src/reunioes/tests/reunioes.service.spec.ts
@@ -7,9 +7,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 describe('Reunioes.service test', () => {
   let service: ReunioesService;
   let prisma: PrismaService;
+  // let reuniao: Reuniao_Processo;
 
   const MockPrismaService = {
-    reunioes: {
+    reuniao_Processo: {
       create: jest.fn(),
       update: jest.fn(),
       delete: jest.fn(),
@@ -30,15 +31,48 @@ describe('Reunioes.service test', () => {
     }).compile();
     service = module.get<ReunioesService>(ReunioesService);
     prisma = module.get<PrismaService>(PrismaService);
-});
+  });
 
-it('os serviços deverão estar definidos', () => {
+  it('os serviços deverão estar definidos', () => {
     expect(service).toBeDefined();
     expect(prisma).toBeDefined();
   });
 
+  it('deverá listar todas as reuniões', async () => {
+    const mockListReunioes: Reuniao_Processo[] = [
+      {
+        id: "M4Hy3w",
+        inicial_id: 111,
+        data_reuniao: new Date("2025-02-05T14:34:21.651Z"),
+        data_processo: new Date("2025-02-05T14:34:21.651Z"),
+        nova_data_reuniao: new Date("2025-02-05T14:34:21.651Z"),
+        justificativa_remarcacao: "queda de energia na data e horário marcado",
+        criado_em: new Date("2025-02-05T14:34:21.651Z"),
+        alterado_em: new Date("2025-02-05T14:34:21.651Z")
+      },
+      {
+        id: "M7Hi3w",
+        inicial_id: 112,
+        data_reuniao: new Date("2025-02-05T14:34:21.651Z"),
+        data_processo: new Date("2025-02-05T14:34:21.651Z"),
+        nova_data_reuniao: new Date("2025-02-05T14:34:21.651Z"),
+        justificativa_remarcacao: "queda de energia na data e horário marcado",
+        criado_em: new Date("2025-02-05T14:34:21.651Z"),
+        alterado_em: new Date("2025-02-05T14:34:21.651Z")
+      }
+    ];
+
+    (prisma.reuniao_Processo.findMany as jest.Mock).mockResolvedValue(mockListReunioes)
+
+    const result = await service.listaCompleta()
+
+    expect(result).not.toBeNull()
+    expect(result).toEqual(mockListReunioes)
+    expect(prisma.reuniao_Processo.findMany).toHaveBeenCalledWith({
+      orderBy:{
+        data_reuniao: 'asc'
+      }
+    })
+
+  })
 });
-
-
-
-
