019e5e1715e165162c332a9f8c7948caeafb02b4
fernando <anjosdelacerda@gmail.com>
2025-07-24 00:48:57 -0300

    feat/alterações em ar-grafico-progressao-mensal

--- src/relatorio/ar-grafico-progressao-mensal/dto/response-relatorio.ts
+++ src/relatorio/ar-grafico-progressao-mensal/dto/response-relatorio.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-
-export class MesDataItem {
+export class MesDataItem { 
     @ApiProperty({
         description: 'Valor de contagem ou total para o mês.',
         example: 0,
@@ -25,34 +24,8 @@ export class MesData {
 
     @ApiProperty({
         description: 'Array contendo o total acumulado para cada mês.',
-        type: [Number],
+        type: [Number], 
         example: [0, 0, 0, 0, 0, 0, 6, 8, 8, 10, 11, 14],
     })
     acc: number[];
-}
-
-
-export class ProgressaoMensalResponseDto {
-    @ApiProperty({
-        description: 'Lista de objetos contendo os dados de progressão mensal para cada ano no período solicitado.',
-        type: [MesData],
-        example: [
-            {
-                "ano": 2024,
-                "mes": [0, 0, 0, 0, 0, 0, 6, 2, 0, 2, 1, 3],
-                "acc": [0, 0, 0, 0, 0, 0, 6, 8, 8, 10, 11, 14]
-            },
-            {
-                "ano": 2025,
-                "mes": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                "acc": [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14]
-            }
-        ]
-    })
-
-    count: MesDataItem;
-    data: MesData;
-
-
-
 }
