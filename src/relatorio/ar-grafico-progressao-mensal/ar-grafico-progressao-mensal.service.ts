8880c399403fe14b38a1a6bb1f2caa637ed04436
fernando lacerda <anjosdelacerda@gmail.com>
2025-10-02 11:33:31 -0300

    feat/implementação de tratativa de erros personalizados em grafico de progressao mensal

--- src/relatorio/ar-grafico-progressao-mensal/ar-grafico-progressao-mensal.service.ts
+++ src/relatorio/ar-grafico-progressao-mensal/ar-grafico-progressao-mensal.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { PeriodFilterDto } from "../relatorio-ar-quantitativo/dto/response-relatorio.dto";
+import { ERROR_MESSAGES } from "./constants/error-messages";
 
 @Injectable()
 export class ArGraficoProgressaoMensalService {
@@ -8,7 +9,8 @@ export class ArGraficoProgressaoMensalService {
 
   async getDataPorAno(periodFilter: PeriodFilterDto) {
 
-    const result = await this.prisma.$queryRaw`
+    try {
+      const resultQuery = await this.prisma.$queryRaw`
   SELECT 
     ano,
     mes,
@@ -26,79 +28,105 @@ export class ArGraficoProgressaoMensalService {
     ano, mes;
     `;
 
-    const anoData: { [key: number]: { ano: number; mes: number[]; acc: number[] } } = {};
+      if (!resultQuery) {
+        throw new HttpException(ERROR_MESSAGES.FALHA_QUERY_BRUTA_INICIAIS_POR_ANO, HttpStatus.INTERNAL_SERVER_ERROR)
+      }
 
-    const anoInicio = new Date(periodFilter.gte).getFullYear();
-    const anoFim = new Date(periodFilter.lte).getFullYear();
+      const anoData: { [key: number]: { ano: number; mes: number[]; acc: number[] } } = {};
 
-    for (let ano = anoInicio; ano <= anoFim; ano++) {
-      anoData[ano] = {
-        ano: ano,
-        mes: Array(12).fill(0),
-        acc: Array(12).fill(0),
-      };
-    }
+      const anoInicio = new Date(periodFilter.gte).getFullYear();
+      const anoFim = new Date(periodFilter.lte).getFullYear();
 
-    if (Array.isArray(result)) {
-      let acc = 0
-      result.forEach((row) => {
-        const ano = Number(row.ano);
-        const mes = Number(row.mes);
-        const total = Number(row.total_registros);
+      for (let ano = anoInicio; ano <= anoFim; ano++) {
+        anoData[ano] = {
+          ano: ano,
+          mes: Array(12).fill(0),
+          acc: Array(12).fill(0),
+        };
+      }
 
-        anoData[ano].mes[mes - 1] = total;
-        acc += total;
-        anoData[ano].acc[mes - 1] = acc;
-      });
-    }
+      if (Array.isArray(resultQuery)) {
+        let acc = 0
+        resultQuery.forEach((row) => {
+          const ano = Number(row.ano);
+          const mes = Number(row.mes);
+          const total = Number(row.total_registros);
 
-    let acumulado = 0;
+          anoData[ano].mes[mes - 1] = total;
+          acc += total;
+          anoData[ano].acc[mes - 1] = acc;
+        });
+      }
 
-    for (let ano = anoInicio; ano <= anoFim; ano++) {
+      let acumulado = 0;
 
-      for (let mes = 0; mes < 12; mes++) {
-        if (anoData[ano].mes[mes] === 0) {
-          anoData[ano].acc[mes] = acumulado;
-        } else {
-          acumulado += anoData[ano].mes[mes];
-          anoData[ano].acc[mes] = acumulado;
+      for (let ano = anoInicio; ano <= anoFim; ano++) {
+
+        for (let mes = 0; mes < 12; mes++) {
+          if (anoData[ano].mes[mes] === 0) {
+            anoData[ano].acc[mes] = acumulado;
+          } else {
+            acumulado += anoData[ano].mes[mes];
+            anoData[ano].acc[mes] = acumulado;
+          }
         }
       }
-    }
 
-    return Object.values(anoData);
+      return Object.values(anoData);
+    } catch (error) {
+      const objectError = {
+        api_mensagem: ERROR_MESSAGES.FALHA_SEGREGAR_INICIAIS_POR_ANO,
+        tipo_erro: error.name,
+        detalhe_tecnico: error.message,
+      };
+      throw new HttpException(
+        objectError,
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
   verificarData(anoInit: string, anoFinal: string): PeriodFilterDto {
-    if (!anoInit && !anoFinal) {
-      return {
-        gte: new Date("2018-01-01"),
-        lte: new Date(),
+    try {
+      if (!anoInit && !anoFinal) {
+        return {
+          gte: new Date("2018-01-01"),
+          lte: new Date(),
+        };
+      }
+
+      if (new Date(anoInit).getFullYear() < new Date("2018").getFullYear()) {
+        throw new HttpException(
+          "A pesquisa de processos está limitada a registros iniciados a partir de 2018. Não é possível consultar processos anteriores a esta data.",
+          HttpStatus.NOT_FOUND
+        )
+      }
+
+      if (new Date(anoFinal).getFullYear() > new Date().getFullYear()) {
+        throw new HttpException(
+          `A pesquisa de processos está limitada aos registros de processos iniciados até o ano de ${new Date().getFullYear()}. Por favor, consulte os processos até essa data. `,
+          HttpStatus.NOT_FOUND
+        )
+      }
+
+      const primeiroDia: Date = new Date(Number(anoInit), Number(0), Number(1));
+      const ultimoDia: Date = new Date(Number(anoFinal), Number(11), Number(31));
+      const periodFilter: PeriodFilterDto = { gte: primeiroDia, lte: ultimoDia };
+      return periodFilter;
+    } catch (error) {
+      const objectError = {
+        api_mensagem: ERROR_MESSAGES.FALHA_VERIFICAR_DATE,
+        tipo_erro: error.name,
+        detalhe_tecnico: error.message,
       };
-    }
-
-    if (new Date(anoInit).getFullYear() < new Date("2018").getFullYear()) {
       throw new HttpException(
-        "A pesquisa de processos está limitada a registros iniciados a partir de 2018. Não é possível consultar processos anteriores a esta data.",
-        HttpStatus.NOT_FOUND
-      )
+        objectError,
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
     }
-
-    if (new Date(anoFinal).getFullYear() > new Date().getFullYear()) {
-      throw new HttpException(
-        `A pesquisa de processos está limitada aos registros de processos iniciados até o ano de ${new Date().getFullYear()}. Por favor, consulte os processos até essa data. `,
-        HttpStatus.NOT_FOUND
-      )
-    }
-
-    const primeiroDia: Date = new Date(Number(anoInit), Number(0), Number(1));
-    const ultimoDia: Date = new Date(Number(anoFinal), Number(11), Number(31));
-    const periodFilter: PeriodFilterDto = { gte: primeiroDia, lte: ultimoDia };
-    return periodFilter
   }
 
   async getRelatorio(anoInit: string, anoFinal: string) {
-    console.log(anoInit, anoFinal)
     const periodFilter: PeriodFilterDto = this.verificarData(anoInit, anoFinal)
     return await this.getDataPorAno(periodFilter)
   }
---
+++ src/relatorio/ar-grafico-progressao-mensal/constants/error-messages.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+    FALHA_QUERY_BRUTA_INICIAIS_POR_ANO: 'Falha ao buscar as iniciais agrupadas por ano',
+    FALHA_SEGREGAR_INICIAIS_POR_ANO: 'Falha ao segregar as iniciais por ano',
+    FALHA_VERIFICAR_DATE: 'Falha ao verificar se a data(date) é válida',
+}
